c6813a4945e337cc073ba78ae9577cd92860c032
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-02-18 00:32:53 +0200

    init

---
+++ app/src/main/java/com/romanivanov/benchmark/MainActivity.java
@@ -0,0 +1,14 @@
+package com.romanivanov.benchmark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
